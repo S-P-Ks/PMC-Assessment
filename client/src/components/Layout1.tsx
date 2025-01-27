84b40d6fb3823c59da3c28ded0f3c24892ed0d72
Shubham Prakash Kumbhare <56545400+S-P-Ks@users.noreply.github.com>
2025-01-27 18:03:32 +0530

    Update Layout1.tsx

--- client/src/components/Layout1.tsx
+++ client/src/components/Layout1.tsx
@@ -2,20 +2,22 @@ import { useGetProductsQuery } from "../store/apis/productQueries";
 import ProductDetails from "./ProductDetails";
 
 function Layout1() {
-  const { data: posts } = useGetProductsQuery({
+  const { data: posts = [] , isFetching} = useGetProductsQuery({
     offset: 0,
     limit: 2,
   });
 
+  if(isFetching){
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="w-full h-screen">
-      <div className="w-full h-1/2">
-        <ProductDetails id={"1"} name={"Product 1"} type={"Technology"} />
-      </div>
-
-      <div className="w-full h-1/2">
-        <ProductDetails id={"1"} name={"Product 1"} type={"Technology"} />
-      </div>
+      {posts.map((post: any, index: number) => (
+        <div key={index} className="w-full h-1/2">
+          <ProductDetails {...post} />
+        </div>
+      ))}
     </div>
   );
 }
