44e5a0de93d119d921e1046cb1a0d44557b04fcf
Shubham Prakash Kumbhare <56545400+S-P-Ks@users.noreply.github.com>
2025-01-27 18:03:50 +0530

    Update Layout2.tsx

--- client/src/components/Layout2.tsx
+++ client/src/components/Layout2.tsx
@@ -2,23 +2,27 @@ import { useGetProductsQuery } from "../store/apis/productQueries";
 import ProductDetails from "./ProductDetails";
 
 function Layout2() {
-  const { data: posts } = useGetProductsQuery({
+  const { data: posts  = [], isFetching} = useGetProductsQuery({
     offset: 2,
     limit: 3,
   });
 
+  if(isFetching){
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="w-full h-screen">
       <div className="w-full h-1/2">
-        <ProductDetails id={"1"} name={"Product 1"} type={"Technology"} />
+        <ProductDetails {...posts[0]} />
       </div>
 
       <div className="w-full h-1/2 flex">
         <div className="w-1/2">
-          <ProductDetails id={"1"} name={"Product 1"} type={"Technology"} />
+          <ProductDetails{...posts[1]}  />
         </div>
         <div className="w-1/2">
-          <ProductDetails id={"1"} name={"Product 1"} type={"Technology"} />
+          <ProductDetails {...posts[2]} />
         </div>
       </div>
     </div>
