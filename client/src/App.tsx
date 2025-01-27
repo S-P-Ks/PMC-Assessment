352f0bd53ab773507fd52aaf47f04a7cad090a17
Shubham Prakash Kumbhare <56545400+S-P-Ks@users.noreply.github.com>
2025-01-27 18:04:47 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,56 @@
+import { useState } from "react";
 import "./App.css";
 import Layout1 from "./components/Layout1";
 import Layout2 from "./components/Layout2";
 import Layout3 from "./components/Layout3";
 import Layout4 from "./components/Layout4";
-import { useGetProductsQuery } from "./store/apis/productQueries";
 
 function App() {
+  const [layoutSelected, setLayoutSelected] = useState(1);
+
+  const renderLayout = () => {
+    switch (layoutSelected) {
+      case 1:
+        return <Layout1 />;
+      case 2:
+        return <Layout2 />;
+      case 3:
+        return <Layout3 />;
+      case 4:
+        return <Layout4 />;
+      default:
+        return <div>No layout selected</div>;
+    }
+  };
   return (
-    <div className="w-full min-h-screen">
-      <Layout1 />
-      <Layout2 />
-      <Layout3 />
-      <Layout4 />
+    <div>
+      <div className="space-x-2 p-4">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setLayoutSelected(1)}
+        >
+          Layout 1
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setLayoutSelected(2)}
+        >
+          Layout 2
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setLayoutSelected(3)}
+        >
+          Layout 3
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setLayoutSelected(4)}
+        >
+          Layout 4
+        </button>
+      </div>
+      {renderLayout()}
     </div>
   );
 }
