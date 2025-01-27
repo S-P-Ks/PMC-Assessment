62f7cb6d3334fd83cfcc674372adf7a8dd0bea92
Shubham Prakash Kumbhare <56545400+S-P-Ks@users.noreply.github.com>
2025-01-27 18:05:20 +0530

    Update product-controller.ts

--- ts-node-express-drizzle/src/controller/product-controller.ts
+++ ts-node-express-drizzle/src/controller/product-controller.ts
@@ -1,7 +1,9 @@
-import { Request, Response } from "express";
-import { findAll } from "../service/product-service.js";
+import { Request, Response } from 'express';
+import { findAll } from '../service/product-service.ts';
 
 export const getAll = async (req: Request, res: Response) => {
-  const products = await findAll();
+  const { offset, limit } = req.body;
+  
+  const products = await findAll(offset, limit);
   res.status(200).json(products);
 };
