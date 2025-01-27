d8aeb422ae6b86fe0f4c0d3a02d0df29aaee9c00
Shubham Prakash Kumbhare <56545400+S-P-Ks@users.noreply.github.com>
2025-01-27 18:04:07 +0530

    Update Layout3.tsx

--- client/src/components/Layout3.tsx
+++ client/src/components/Layout3.tsx
@@ -2,23 +2,27 @@ import { useGetProductsQuery } from "../store/apis/productQueries";
 import ProductDetails from "./ProductDetails";
 
 function Layout3() {
-  const { data: posts } = useGetProductsQuery({
+  const { data: posts ,isFetching} = useGetProductsQuery({
     offset: 5,
     limit: 3,
   });
+  
+  if(isFetching){
+    return <div>Loading...</div>
+  }
 
   return (
     <div className="w-full h-screen">
       <div className="w-full h-1/3">
-        <ProductDetails id={"1"} name={"Product 1"} type={"Technology"} />
+        <ProductDetails {...posts[0]}  />
       </div>
 
       <div className="w-full h-1/3">
-        <ProductDetails id={"1"} name={"Product 1"} type={"Technology"} />
+        <ProductDetails {...posts[1]} />
       </div>
 
       <div className="w-full h-1/3">
-        <ProductDetails id={"1"} name={"Product 1"} type={"Technology"} />
+        <ProductDetails {...posts[2]}  />
       </div>
     </div>
   );
